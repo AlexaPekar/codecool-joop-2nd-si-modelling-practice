4309939ae313dc37b883bcb024c9a7c06f5d8c49
AlexaPekar <missdreambelle@gmail.com>
2018-01-20 13:01:03 +0100

    Created basic Thing.java to contain common attributes of things in the castle

---
+++ src/com/codecool/Thing.java
@@ -0,0 +1,14 @@
+package com.codecool;
+
+public class Thing {
+
+    protected String name;
+
+    protected Thing(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
