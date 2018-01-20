55d74e2402c45745abab186db748f61324d3a8d7
AlexaPekar <missdreambelle@gmail.com>
2018-01-20 12:58:05 +0100

    Created basic Man.java, Woman.java as types of characters

---
+++ src/com/codecool/Man.java
@@ -0,0 +1,8 @@
+package com.codecool;
+
+public class Man extends Character {
+    
+    public Man(String name, Gender gender, int bravery, int stamina, int energy, Tool[] tools) {
+        super(name, gender, 100, 80, energy, tools);
+    }
+}
---
+++ src/com/codecool/Woman.java
@@ -0,0 +1,8 @@
+package com.codecool;
+
+public class Woman extends Character {
+
+    public Man(String name, Gender gender, int bravery, int stamina, int energy, Tool[] tools) {
+        super(name, gender, 80, 100, energy, tools);
+    }
+}
