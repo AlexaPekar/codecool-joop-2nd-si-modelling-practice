ca72da6cb4a26e3865ed8313d8ee12417c5de3a4
AlexaPekar <missdreambelle@gmail.com>
2018-01-29 18:23:34 +0100

    Added weight field, added getters for fields

--- src/com/codecool/Furniture.java
+++ src/com/codecool/Furniture.java
@@ -4,7 +4,17 @@ public class Furniture extends Thing { //abstract
 
     int weight;
 
-    protected Furniture(String name) {
+    public Furniture(String name, int weight) {
         super(name);
+        this.weight = weight;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
 }
