f407ff9140ac9366114ed1bf8604cc774854a69b
AlexaPekar <missdreambelle@gmail.com>
2018-01-29 18:26:20 +0100

    Fixes methods based on serialization, created pickUp and putDown methods to handle furnitures

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,83 +1,123 @@
 package com.codecool;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.io.File;
 
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("../data/characters.ser");
         Castle newCastle = Castle.createNewCastle();
-        System.out.println("Available commands: :list, :create, :find, :genders, :exit");
+        if (f.exists()) {
+            newCastle.deserializeCharacters();
+        }
+        System.out.println(newCastle);
+        System.out.println("Available commands: :listCharacters, :listPlaces :create, :find, :exit, :pickUpFurniture, :putDownFurniture, :chooseCharacter");
         while (true) {
             String line = scanner.nextLine();
             if (":exit".equals(line)) {
                 break;
 
-            } else if (":list".equals(line)) {
-                handleList(newCastle.getCharacters());
+            } else if (":listCharacters".equals(line)) {
+                handleListCharacters(newCastle.getCharacters());
 
-            } else if (":genders".equals(line)) {
-                handleGenders();
+            } else if (":listPlaces".equals(line)) {
+                handleListPlaces(newCastle.getPlaces());
 
             } else if (":create".equals(line)) {
                 handleCreate(newCastle);
 
             } else if (":find".equals(line)) {
                 handleFind(newCastle);
+
+            } else if (":chooseCharacter".equals(line)) {
+                handleChooseCharacter(newCastle);
+
+            } else if ("pickUpFurniture".equals(line)) {
+                pickUpFurniture(newCastle);
+
+            } else if ("putDownFurniture".equals(line)) {
+                putDownFurniture(newCastle);
             }
         }
         newCastle.quit();
     }
 
-    private static void handleList(Character[] characters) {
-        System.out.println("These characters are in the castle: ");
+    private static void handleChooseCharacter(Castle newCastle) {
+        System.out.println("Please, enter the name of the character, you wanna play with!");
+        String name = scanner.nextLine();
+        Character myCharacter = newCastle.findCharacter(name);
+        if (myCharacter == null) {
+            System.out.println("There's no such character in the castle!");
+        } else {
+            newCastle.setActiveCharacter(myCharacter);
+            System.out.println("You choosed your character!");
+        }
+    }
+
+    private static void handleListCharacters(Character[] characters) {
         if (characters.length == 0) {
             System.out.println("Nobody is in the castle.");
         } else {
+            System.out.println("These characters are in the castle: ");
             for (Character character : characters) {
-                System.out.println("\t" + "Name: " + character.getName() + "    Energy: " + character.getEnergy() + "    Strength: " +character.getStrength());
+                System.out.println("\tName: " + character.getName() + "\tEnergy: " + character.getEnergy() + "\tStrength: " +character.getStrength() + "\tStocks: " + character.getStocks());
             }
         }
     }
 
-    private static void handleGenders() {
-        System.out.println("The available genders to enter the castle: (case-sensitive)");
-        for (Gender gender : Gender.values()) {
-            System.out.println("\t" + gender);
+    private static void handleListPlaces(Place[] places) {
+        if (places.length == 0) {
+            System.out.println("The castle is empty.");
+        } else {
+            System.out.println("These places are in the castle: ");
+            for (Place place : places) {
+                System.out.println("\t" + "Name: " + place.getName() + "\tFurniture: " + Arrays.toString(place.getFurniture()));
+            }
         }
     }
 
     private static void handleCreate(Castle newCastle) {
         System.out.println("Please, enter the name of the character!");
         String name = scanner.nextLine();
-
-        System.out.println("Please, enter the gender of your character! (see :genders)");
-        String gender = scanner.nextLine();
-
-
-        Character newCharacter = newCastle.createCharacter(name, 100, 0);
-
-        newCastle.addToCharacters(newCharacter);
-
+        newCastle.createCharacter(name, 100, 0, new Furniture[0]);
         System.out.println("Your character is created.");
     }
 
     private static void handleFind(Castle newCastle) {
         System.out.println("Please, enter the name of the character, you wanna find!");
         String name = scanner.nextLine();
-
         Character character = newCastle.findCharacter(name);
         if (character == null) {
             System.out.println("There's no such character in the castle!");
         } else {
-            System.out.format("This character is found!    Name: " + character.getName() + "    Energy: " + character.getEnergy() + "    Strength: " +character.getStrength(), character.getName());
+            System.out.println("This character is found!\tName: " + character.getName() + "\tEnergy: " + character.getEnergy() + "\tStrength: " + character.getStrength() + "\tStocks: " + character.getStocks());
         }
     }
 
+    private static void pickUpFurniture(Castle newCastle) {
+        System.out.println("Add index of Place!");
+        int indexPlace = scanner.nextInt();
+        System.out.println("Add index of Furniture!");
+        int indexFurniture = scanner.nextInt();
+        Place[] tempPlaces = newCastle.getPlaces();
+        Furniture[] tempFurniture = tempPlaces[indexPlace].getFurniture();
+        Furniture newFurniture = Place.removeFurniture(tempFurniture, indexFurniture);
+        newCastle.activeCharacter.addNewStock(newFurniture);
+    }
 
-
-
-
+    private static void putDownFurniture(Castle newCastle) {
+        System.out.println("Add index of Place!");
+        int indexPlace = scanner.nextInt();
+        System.out.println("Add index of Furniture!");
+        int indexFurniture = scanner.nextInt();
+        Furniture newFurniture = newCastle.activeCharacter.removeStock(indexFurniture);
+        Place[] tempPlaces = newCastle.getPlaces();
+        Furniture[] tempFurniture = tempPlaces[indexPlace].getFurniture();
+        Place.addNewFurniture(tempFurniture, newFurniture);
+    }
 }
