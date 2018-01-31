e81a3051bd0efa2b773b66bf6090b1ef989b072b
AlexaPekar <missdreambelle@gmail.com>
2018-01-31 23:16:37 +0100

    Mixed up furniture of places in Place class

--- src/com/codecool/Place.java
+++ src/com/codecool/Place.java
@@ -17,55 +17,55 @@ public class Place extends Thing {
 
     public static Place createKitchen() {
         Furniture[] furniture = new Furniture[4];
-        furniture[0] = new Furniture("table", 10);
-        furniture[1] = new Furniture("chair", 4);
-        furniture[2] = new Furniture("cooker", 50);
-        furniture[3] = new Furniture("fridge", 40);
+        furniture[0] = new Furniture("toilet", 20);
+        furniture[1] = new Furniture("bed", 50);
+        furniture[2] = new Furniture("mirror", 5);
+        furniture[3] = new Furniture("piano", 80);
         return new Place("Kitchen", furniture);
     }
 
     public static Place createBedroom() {
         Furniture[] furniture = new Furniture[4];
-        furniture[0] = new Furniture("bed", 50);
-        furniture[1] = new Furniture("bedside cabinet", 5);
-        furniture[2] = new Furniture("night light", 2);
-        furniture[3] = new Furniture("wardrobe", 50);
+        furniture[0] = new Furniture("chair", 4);
+        furniture[1] = new Furniture("rope", 10);
+        furniture[2] = new Furniture("stationary bike", 30);
+        furniture[3] = new Furniture("desk", 20);
         return new Place("Bedroom", furniture);
     }
 
     public static Place createBathroom() {
         Furniture[] furniture = new Furniture[4];
-        furniture[0] = new Furniture("toilet", 20);
-        furniture[1] = new Furniture("sink", 10);
-        furniture[2] = new Furniture("mirror", 5);
-        furniture[3] = new Furniture("tub", 30);
+        furniture[0] = new Furniture("table", 10);
+        furniture[1] = new Furniture("bookshelf", 10);
+        furniture[2] = new Furniture("fireplace", 50);
+        furniture[3] = new Furniture("cooker", 50);
         return new Place("Bathroom", furniture);
     }
 
     public static Place createLibrary() {
         Furniture[] furniture = new Furniture[4];
-        furniture[0] = new Furniture("bookshelf", 10);
-        furniture[1] = new Furniture("desk", 20);
-        furniture[2] = new Furniture("table lamp", 2);
-        furniture[3] = new Furniture("armchair", 30);
+        furniture[0] = new Furniture("sink", 10);
+        furniture[1] = new Furniture("treadmill", 40);
+        furniture[2] = new Furniture("bedside cabinet", 5);
+        furniture[3] = new Furniture("coffe table", 10);
         return new Place("Library", furniture);
     }
 
     public static Place createLivingroom() {
         Furniture[] furniture = new Furniture[4];
-        furniture[0] = new Furniture("sofa", 50);
-        furniture[1] = new Furniture("fireplace", 50);
-        furniture[2] = new Furniture("piano", 80);
-        furniture[3] = new Furniture("coffe table", 10);
+        furniture[0] = new Furniture("tub", 30);
+        furniture[1] = new Furniture("wall bars", 50);
+        furniture[2] = new Furniture("fridge", 40);
+        furniture[3] = new Furniture("table lamp", 2);
         return new Place("Living room", furniture);
     }
 
     public static Place createTrainingGround() {
         Furniture[] furniture = new Furniture[4];
-        furniture[0] = new Furniture("rope", 10);
-        furniture[1] = new Furniture("treadmill", 40);
-        furniture[2] = new Furniture("stationary bike", 30);
-        furniture[3] = new Furniture("wall bars", 50);
+        furniture[0] = new Furniture("night light", 2);
+        furniture[1] = new Furniture("armchair", 30);
+        furniture[2] = new Furniture("sofa", 50);
+        furniture[3] = new Furniture("wardrobe", 50);
         return new Place("Training ground", furniture);
     }
 
